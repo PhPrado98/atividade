066231684963ede7c423b7710667ff9b23308b14
PhPrado98 <pedro.prado.pinheiro@escola.pr.gov.br>
2025-03-26 10:48:15 -0300

    Add files via upload

---
+++ atividade 5 - main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    float real, dolar;
+    scanf ("%f", &real);
+    dolar = (real * 5.9693);
+    printf ("dolar = %.2f, real = %.2f", dolar, real );
+    return 0;
+}
